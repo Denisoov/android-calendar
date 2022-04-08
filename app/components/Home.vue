f18a109067cc8350bd69a4ba5f6a4c9f0aeb3693
Denisoov <denisoov07@gmail..com>
2022-04-09 00:31:04 +0500

    добавил календарь

---
+++ app/components/Home.vue
@@ -0,0 +1,30 @@
+<template>
+    <Page>
+    <ActionBar :title="title">
+      <NavigationButton text="Back" ></NavigationButton>
+    </ActionBar>
+    <RadCalendar></RadCalendar>
+  </Page>
+</template>
+
+<script>
+
+  export default {
+
+  };
+</script>
+
+<style scoped lang="scss">
+    @import '@nativescript/theme/scss/variables/blue';
+
+    // Custom styles
+    .fas {
+        @include colorize($color: accent);
+    }
+
+    .info {
+        font-size: 20;
+        horizontal-align: center;
+        vertical-align: center;
+    }
+</style>
