94eac27dad5a3d47c19acb8724b73cca7b6a33bb
Denisoov <denisoov07@gmail..com>
2022-04-10 13:53:00 +0500

    Версткая панели управления

--- app/app.js
+++ app/app.js
@@ -1,8 +1,10 @@
 import Vue from 'nativescript-vue'
 
 import Home from './components/Home'
-import CalendarView from 'nativescript-ui-calendar/vue';
-Vue.use(CalendarView);
+
+import VueDevtools from 'nativescript-vue-devtools'
+
+Vue.use(VueDevtools, { host: '192.168.0.101'})
 
 new Vue({
   render: (h) => h('frame', [h(Home)]),
--- app/components/Home.vue
+++ app/components/Home.vue
@@ -1,32 +1,56 @@
 <template>
-    <Page>
-    <ActionBar :title="title">
-      <NavigationButton text="Back" ></NavigationButton>
-    </ActionBar>
-    <RadCalendar
-        locale="ru-RU"
-    ></RadCalendar>
+  <Page>
+    <StackLayout class="calendar-wrapper">
+      <FlexboxLayout height="50" class="calendar-panel">
+        <Button 
+          text="<" 
+          class="calendar-panel__prev"
+          width="30"
+          height="30"
+        />
+        <Label class="calendar-panel__mounth" :text="month" width="120" height="30"/>
+        <Button 
+          text=">"
+          class="calendar-panel__next"
+          width="30"
+          height="30"
+        />
+      </FlexboxLayout>
+      <Label text="second" height="70" backgroundColor="#289062"/>
+      <Label text="third" height="70" backgroundColor="#1c6b48"/>
+    </StackLayout>
   </Page>
 </template>
 
 <script>
-
   export default {
-
+    data: () => ({
+      typeChart: 'lastMounth',
+      nowDay: new Date().getDate(),
+      nowMonth: new Date().getMonth(),
+      nowYear: new Date().getFullYear(),
+      selectedDay: new Date().getDate(),
+      month: new Date().getMonth(),
+      year: new Date().getFullYear(),
+      monthes: ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"],
+      date: new Date(),
+    })
   };
 </script>
 
-<style scoped lang="scss">
-    @import '@nativescript/theme/scss/variables/blue';
-
-    // Custom styles
-    .fas {
-        @include colorize($color: accent);
+<style lang="scss">
+  .calendar-wrapper {
+    .calendar-panel {
+      align-items: center;
+      justify-content: center;
     }
-
-    .info {
-        font-size: 20;
-        horizontal-align: center;
-        vertical-align: center;
+    .calendar-panel__prev, .calendar-panel__next {
+      border-radius: 50%;
+      font-weight: 600;
     }
+    .calendar-panel__mounth {
+      padding-top: 18px;
+      text-align: center;
+    }
+  }
 </style>
